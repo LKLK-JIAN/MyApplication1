713dda8a5ca8b5a0f7e9e771555fc4f2fac017ae
老盖 <1047601623@qq.com>
2017-07-10 02:06:09 +0800

    改动了toast2

--- app/src/main/java/com/example/myapplication1/Activity.java
+++ app/src/main/java/com/example/myapplication1/Activity.java
@@ -24,6 +24,7 @@ public class Activity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(Activity.this, "", Toast.LENGTH_SHORT).show();
+
             }
         });
 
